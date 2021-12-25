e251812f9a6be6a106c291e0a483b669e96bb268
Shittu-Moshood <shittumoshoodademola@gmail.com>
2021-12-25 16:05:33 +0100

    second

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+
+<template>
+  <div id="nav">
+  <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <img src="../assets/pp.jpeg" alt="" style="height:30px; width:30px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="navbar-nav mr-auto">
+            <router-link to="/" class="nav-item nav-link">Home</router-link>
+            <router-link to="/about" class="nav-item nav-link">About</router-link>
+            <router-link to="/services" class="nav-link">Services</router-link>
+            <router-link to="/contact" class="nav-link">Contact</router-link>
+        </div>
+        
+    </div>
+</nav>
+ </div>
+  <router-view/>
+</template>
+
+
+
+<style scoped>
+    
+    nav{
+    
+    display: flex;
+    justify-content: space-between;
+    }
+    #nav a {
+    font-weight: bold;
+    color: black;
+    }
+
+    #nav a.router-link-exact-active {
+    color: black;
+    background: crimson;
+    border-radius: .5rem;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,32 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/navbar',
+    name: 'Navbar',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/Navbar.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Services.vue')
+  },
+  
 ]
 
 const router = createRouter({
